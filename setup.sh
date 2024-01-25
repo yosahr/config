ce46d5141b8efe1094007d6cb564dcc4bd80e0a6
Yohanan Romem <john.romem@dell.com>
2024-01-25 13:51:36 +0000

    Init

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git config --global push.autoSetupRemote true
+
+
+# add repo for git to apt i.e get latest version
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
